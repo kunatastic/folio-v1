7a76d0cf9a3d9887ca06c79bada390eb469600ce
kunatastic <kjha6666@gmail.com>
2021-01-29 23:33:55 +0530

    aboutme footer

--- again-1/script/script.js
+++ again-1/script/script.js
@@ -1,3 +1,5 @@
+// * SHADOW ON NAVIGATION * //
+
 window.onscroll = function () {
   scrollFunction();
 };
@@ -10,3 +12,49 @@ function scrollFunction() {
       "0 0 0 rgba(0,0,0,0)";
   }
 }
+
+// * SIDEBAR TOGGLE * //
+
+var $xBar = $(".close"),
+  $sideNav = $(".side-nav"),
+  $menuIcon = $(".hamburger");
+
+$menuIcon.click(function () {
+  $sideNav.css({
+    left: 0,
+  });
+  $menuIcon.fadeOut(5);
+  $xBar.css({
+    top: 30,
+  });
+  if ($(window).width() < 767) {
+    $sideNav.css({
+      left: 0,
+    });
+  }
+  if ($(window).width() < 992) {
+    $sideNav.css({
+      width: 270,
+    });
+  }
+});
+
+$xBar.click(function () {
+  $sideNav.css({
+    left: -307,
+  });
+  $menuIcon.fadeIn(500);
+  $xBar.css({
+    top: -30,
+  });
+  if ($(window).width() < 767) {
+    $sideNav.css({
+      left: -100 + "%",
+    });
+  }
+  if ($(window).width() < 991 && $(window).width() > 1199) {
+    $sideNav.css({
+      left: -270,
+    });
+  }
+});
