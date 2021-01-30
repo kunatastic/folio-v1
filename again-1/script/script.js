f243c94b9dd862eecb1cadc93a3c0baa9ffae1cd
kunatastic <kjha6666@gmail.com>
2021-01-30 19:18:03 +0530

    aboutme footer

--- again-1/script/script.js
+++ again-1/script/script.js
@@ -1,4 +1,4 @@
-// * SHADOW ON NAVIGATION * //
+//======================================== * SHADOW ON NAVIGATION * ==================================================//
 
 window.onscroll = function () {
   scrollFunction();
@@ -13,48 +13,19 @@ function scrollFunction() {
   }
 }
 
-// * SIDEBAR TOGGLE * //
+//======================================== * SIDEBAR TOGGLE * ==================================================//
 
-var $xBar = $(".close"),
-  $sideNav = $(".side-nav"),
-  $menuIcon = $(".hamburger");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-$menuIcon.click(function () {
-  $sideNav.css({
-    left: 0,
-  });
-  $menuIcon.fadeOut(5);
-  $xBar.css({
-    top: 30,
-  });
-  if ($(window).width() < 767) {
-    $sideNav.css({
-      left: 0,
-    });
-  }
-  if ($(window).width() < 992) {
-    $sideNav.css({
-      width: 270,
-    });
-  }
+hamburger.addEventListener("click", () => {
+  console.log("CLCKKEDD");
 });
 
-$xBar.click(function () {
-  $sideNav.css({
-    left: -307,
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
   });
-  $menuIcon.fadeIn(500);
-  $xBar.css({
-    top: -30,
-  });
-  if ($(window).width() < 767) {
-    $sideNav.css({
-      left: -100 + "%",
-    });
-  }
-  if ($(window).width() < 991 && $(window).width() > 1199) {
-    $sideNav.css({
-      left: -270,
-    });
-  }
 });
